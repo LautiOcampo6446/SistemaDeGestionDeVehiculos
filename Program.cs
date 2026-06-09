b8d7d743924808a1b510f9f509d333ab4b551a6a
LautiOcampo6446 <lautarobenjaminocampo@yahoo.com.ar>
2026-06-09 20:13:40 -0300

    Agregue una lista, do-while y switch

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
-
+using System.Collections.Generic;
 public class Program
 {
+    static List<Vehiculo> listaVehiculos = new List<Vehiculo>();
     static void Main()
     {
-        
+        int opcion = -1;
+        do
+        {
+            Console.WriteLine(@"SISTEMA DE GESTIÓN DE VEHÍCULOS 
+            Ingrese una opción:
+            1. Agregar Auto
+            2. Agregar Moto
+            3. Listar todos los vehículos
+            0. Salir");
+            switch (opcion)
+            {
+                case 1:
+                    AgregarAuto();
+                    break;
+                case 2:
+                    AgregarMoto();
+                    break;
+                case 3:
+                    ListarVehiculos();
+                    break;
+                case 0:
+                    Console.WriteLine("Saliendo del sistema");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        } while (opcion != 0);
     }
+    
 }
+
